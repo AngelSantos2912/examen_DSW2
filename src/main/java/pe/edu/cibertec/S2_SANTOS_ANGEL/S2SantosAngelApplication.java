2eb46bf9fb59750b5db63cbbd4af959825e55b91
AngelSantos2912 <angelleonardo975533406@gmail.com>
2024-11-10 09:57:43 -0500

    examen

---
+++ src/main/java/pe/edu/cibertec/S2_SANTOS_ANGEL/S2SantosAngelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.S2_SANTOS_ANGEL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2SantosAngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2SantosAngelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/S2_SANTOS_ANGEL/S2SantosAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.S2_SANTOS_ANGEL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2SantosAngelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
